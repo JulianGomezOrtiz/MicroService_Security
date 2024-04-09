84a688ba3f186f6aae2dadf78aab9cbbe5ffca85
Julian <juliangomezortiz.05@gmail.com>
2024-04-08 20:34:42 -0500

    Creación del repositorio del microservicio seguridad

---
+++ src/main/java/com/ucaldas/mssecurity/Models/User.java
@@ -0,0 +1,94 @@
+package com.ucaldas.mssecurity.Models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class User {
+    @Id
+    private String _id;
+    private  String email;
+    private String name;
+    private String password;
+    private Date created_at = new Date();
+    private boolean isBlocked = false;
+    private two_factor_code = two_factor_code;
+    @DBRef
+    private Role role;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+    public String get_id() {
+        return _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public boolean isBlocked() {
+        return isBlocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        isBlocked = blocked;
+    }
+
+    public String getTwo_factor_code() {
+        return two_factor_code;
+    }
+
+    public void setTwo_factor_code(String two_factor_code) {
+        this.two_factor_code = two_factor_code;
+    }
+
+
+
+}
