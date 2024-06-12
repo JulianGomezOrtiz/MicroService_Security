debdf1cd8b2bb19997804619b8cf9f8691930b99
Julian <juliangomezortiz.05@gmail.com>
2024-06-12 01:14:05 -0500

    se permite el uso del microservicio desde el puerto 4200(frontend)

--- src/main/java/com/ucaldas/mssecurity/Configurations/WebConfig.java
+++ src/main/java/com/ucaldas/mssecurity/Configurations/WebConfig.java
@@ -2,21 +2,31 @@ package com.ucaldas.mssecurity.Configurations;
 
 import com.ucaldas.mssecurity.Interceptors.SecurityInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Autowired
     private SecurityInterceptor securityInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-
-       // registry.addInterceptor(securityInterceptor)
+        // registry.addInterceptor(securityInterceptor)
         // .addPathPatterns("/api/**")
-              //  .excludePathPatterns("/api/public/**");
+        // .excludePathPatterns("/api/public/**");
+    }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
